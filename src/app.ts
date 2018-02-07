081312bd4baedd83eb03e20edc33aa9304526019
ryanvolum <ryanvolum@gmail.com>
2018-02-07 15:49:54 -0800

    Initial Middleware Push

---
+++ src/app.ts
@@ -0,0 +1,90 @@
+import { Bot, MemoryStorage, BotStateManager, ConsoleLogger } from 'botbuilder';
+import { BotFrameworkAdapter } from 'botbuilder-services';
+import { translate } from './translate-middleware';
+import * as restify from "restify";
+import { LuisRecognizer } from 'botbuilder-ai';
+
+// Create server
+let server = restify.createServer();
+server.listen(process.env.port || process.env.PORT || 3978, function () {
+    console.log(`${server.name} listening to ${server.url}`);
+});
+
+// Create adapter and listen to servers '/api/messages' route.
+const adapter = new BotFrameworkAdapter({
+    appId: process.env.MICROSOFT_APP_ID,
+    appPassword: process.env.MICROSOFT_APP_PASSWORD
+});
+server.post('/api/messages', <any>adapter.listen());
+
+interface translationPreferences {
+    toLanguageCode: string,
+    fromLanguageCode: string
+}
+
+const getLanguagePreferences = (context: BotContext): void | translationPreferences => {
+    if (context.request.type === 'message' && context.state.conversation.translationPreferences) {
+        return context.state.conversation.translationPreferences;
+    }
+}
+
+const getActiveLanguage = (context: BotContext): string => {
+    if (context.request.type === 'message' && context.state.user.translateTo) {
+        return context.state.user.translateTo;
+    } else {
+        return null;
+    }
+}
+
+const languageMap = {
+    "danish": "da", "dutch": "nl", "english": "en", "finnish": "fi", "french": "fr", "german": "de", "greek": "el", "italian": "it", "japanese": "ja", "norwegian": "no", "polish": "pl", "portuguese": "pt", "russian": "ru", "spanish": "es", "swedish": "sv", "turkish": "tr"
+};
+
+const isSupportedLanguage = (language) => {
+    return languageMap.hasOwnProperty(language.toLowerCase());
+}
+
+const getLanguageCode = (language) => {
+    return languageMap[language];
+}
+
+const getTopIntent = (context: BotContext) => {
+    return (context.topIntent && context.topIntent.name) ? context.topIntent.name : null;
+}
+
+const getTopEntity = (context: BotContext) => {
+    return (context.topIntent && context.topIntent.entities && context.topIntent.entities[0]) ? context.topIntent.entities[0] : null;
+}
+
+const setActiveLanguage = (context: BotContext, language): void => {
+    context.state.user.translateTo = getLanguageCode(language);
+}
+
+const bot = new Bot(adapter)
+    .use(new ConsoleLogger())
+    .use(new MemoryStorage())
+    .use(new BotStateManager())
+    .use(new translate("5fa547f29f94485e9eeb78a7f393adf7", "en", getActiveLanguage))
+    .use(new LuisRecognizer('029ad101-c978-4bbe-b2ae-e95c193ad580', '9c33ab53fea54a71831fa4098fa845a3'))
+    .onReceive((context) => {
+        if (context.request.type === 'message') {
+
+            if (getTopIntent(context) === 'changeLanguage') {
+                var entity = getTopEntity(context);
+
+                if (entity.type === 'language::toLanguage') {
+
+                    if (isSupportedLanguage(entity.value)) {
+                        setActiveLanguage(context, entity.value);
+                        context.reply(`Changing your language to ${entity.value}`);
+                    } else {
+                        context.reply(`${entity.value} is not a supported language.`);
+                    }
+                } else {
+                    context.reply(`You have to tell me what language to translate to!`);
+                }
+            }
+            context.reply(`You just said:`).reply(`"${context.request.text}"`);
+        }
+
+    });
---
+++ src/translate-middleware.ts
@@ -0,0 +1,77 @@
+import { Middleware, Activity, ConversationResourceResponse, ActivityAdapter } from "botbuilder";
+import { LuisRecognizer } from "botbuilder-ai";
+import * as cs from 'cognitive-services';
+
+interface translationPreferences {
+    toLanguageCode: string,
+    fromLanguageCode: string
+}
+
+export class translate implements Middleware {
+    private translationKey: string;
+    private botLanguage: string;
+    private getPreferences: (context: BotContext) => string;
+
+    constructor(translationKey: string, botLanguage: string, getPreferences: (c: BotContext) => string) {
+        this.translationKey = translationKey;
+        this.botLanguage = botLanguage;
+        //assuming synchronous for now...
+        this.getPreferences = getPreferences;
+    }
+
+    public receiveActivity(context: BotContext, next: () => Promise<void>): Promise<void> {
+        let language = this.getPreferences(context);
+        if (language) {
+            return this.translate(context.request.text, language, this.botLanguage)
+                .then(response => {
+                    context.request.text = response;
+                    return next();
+                }).catch(err => {
+                    console.warn(err);
+                    return next();
+                });
+        } else {
+            return next();
+        }
+    }
+    //TODO: use batch translation api...
+    public postActivity(context: BotContext, activities: Partial<Activity>[], next: () => Promise<ConversationResourceResponse[]>): Promise<ConversationResourceResponse[]> {
+        let language = this.getPreferences(context);
+        if (language) {
+            return Promise.all(
+                activities
+                    .map(activity => {
+                        return this.translate(activity.text, this.botLanguage, language)
+                    })
+            )
+                .then(res => {
+                    res.forEach((translation, i) => {
+                        activities[i].text = translation;
+                    })
+                    return next();
+                }).catch(err => {
+                    console.warn(err);
+                    return next();
+                })
+        } else {
+            return next();
+        }
+    }
+
+    //switches to and from
+    public translate = (text: string, from: string, to: string): Promise<string> => {
+        const translationClient = new cs.textTranslator({
+            apiKey: this.translationKey
+        });
+        const parameters = {
+            from: from,
+            to: to,
+            text: text,
+            contentType: "text/plain"
+        };
+        return translationClient.translate({ parameters });
+    }
+}
+
+
+
